0ae3d5f7d0d79fe0ddb249052936d494c7d02bae
Nilesh Deb <debnilesh69@gmail.com>
2026-02-23 00:56:39 +0530

    write models.py, serializers.py,urls.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from .models import Note
 # Create your serializers here.
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,3 +12,9 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,3 +6,7 @@ from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
