18388c3ab0db6474f12d3b71dee0ff0ce345df4a
Nilesh Deb <debnilesh69@gmail.com>
2026-02-25 00:47:36 +0530

    add frontend

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,8 +2,32 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer,NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note 
+
+class NoteListCreateView(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user) 
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else: 
+            print(serializer.errors)
+
+class NoteDeleteView(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user) 
+
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
